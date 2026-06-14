153920e83a148ecc6717b2698277bca4eb30e715
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-06-14 19:34:41 +0300

    feat : add todos crud with minimal api

--- Learn_Cqrs.Api/Program.cs
+++ Learn_Cqrs.Api/Program.cs
@@ -2,6 +2,8 @@ using FluentValidation;
 using Learn_Cqrs.Application;
 using Learn_Cqrs.Application.Behavior;
 using Learn_Cqrs.Application.Common.Interfaces;
+using Learn_Cqrs.Application.Todos.Commands.CreateTodo;
+using Learn_Cqrs.Application.Todos.Queries.GetTodos;
 using Learn_Cqrs.Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -15,7 +17,39 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data S
 builder.Services.AddScoped<IAppDbContext , AppDbContext>(); 
 
 var app = builder.Build();
+var api = app.MapGroup("/api"); 
 
-app.MapGet("/", () => "Hello World!");
+var todosGroup = api.MapGroup("/todos");
+todosGroup.MapGet("/", async (IMediator mediator) =>
+{
+   var result = await  mediator.Send(new GetTodosQuery()); 
+   return TypedResults.Ok(result);  
+});
 
+todosGroup.MapGet("/{id:guid}", async (IMediator mediator, Guid id)  =>
+{
+    var result = await mediator.Send(new GetTodoQuery (id));
+    return result is null ? Results.NotFound() : TypedResults.Ok(result);
+});
+
+todosGroup.MapPost("/", async (IMediator mediator , CreateTodoCommand command) =>
+{
+    var result = await mediator.Send(command);
+    return TypedResults.Ok( new
+    {
+        Id = result 
+    });
+});
+
+
+todosGroup.MapDelete("/{id:guid}", async (IMediator mediator, Guid id) =>
+{
+   await mediator.Send(new DeleteTodoCommand(id)  );
+    return TypedResults.Ok(); 
+});
+todosGroup.MapPut("/", async (IMediator mediator, UpdateTodoCommand command) =>
+{
+    await mediator.Send( command);
+    return TypedResults.Ok();
+});
 app.Run();
